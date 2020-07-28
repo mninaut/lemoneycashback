09c851f78e0889281f3510316362faf4b722c15c
mninaut <m@ninaut.com>
2020-07-28 06:48:43 -0300

    fix query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    datetime = Time.now.localtime
-    offers = Portal::Offer.where("starts_at >= '#{datetime}'" +  "OR ends_at < '#{datetime}'"+  "OR ends_at is NULL").order(premium: :desc)
+    datetime = Time.now
+    offers = Portal::Offer.where("starts_at <= '#{datetime}'" + "AND ends_at >= '#{datetime}'").order(premium: :desc)
     
     gon.offers = offers
   end
